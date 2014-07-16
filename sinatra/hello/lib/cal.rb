8f99e5b5ffe056dde762eba8e163cde5360e082f
Sujitha <sujitha@railsfactory.org>
2014-07-16 14:40:32 +0530

    Modified Sinatra for Finding Day

--- sinatra/hello/lib/cal.rb
+++ sinatra/hello/lib/cal.rb
@@ -1,14 +1,24 @@
 require 'sinatra'
 require './roman'
 require './numbers'
-require './spelldigit'
+require './spelldigit' 
+require './day'
+
 get '/page' do
-month = params['month']
-day = params['date']
-year = params['year']
+month = params['month'].to_i
+day = params['date'].to_i
+year = params['year'].to_i
 page = File.read('./form1.html')
 s = Roman.convert(year)
 d = Numbers.convert(day)
 spell = Spell_digit.convert(year)
-page = page + month.to_s + "\s" + d.to_s + "\s" + year.to_s + "\s" + s + "\s" + spell
+x = ""
+if month!=0 && day!=0 && year!=0
+t = Time.new(year,month,day)
+x = t.strftime('%A')
+a = Day.convert(x)
+end
+page = page + d.to_s + "\s" + month.to_s + "\s" + year.to_s + "\s" + s + "\s" + spell + "\s" 
+page = page + x + "\n" + a.to_s
+
 end
---
+++ sinatra/hello/lib/day.rb
@@ -0,0 +1,22 @@
+class Day
+ def self.convert(input)
+ input = input.to_s
+  if input == "Monday"
+    "Thingal"
+  elsif input == "Tuesday"
+    "Shevvai"
+  elsif input == "Wednesday"
+    "Phuthan"
+  elsif input == "Thursday"
+    "Viyalan"
+  elsif input == "Friday"
+    "Velli"
+  elsif input == "Saturday"
+    "Saeni"
+  else input == "Sunday"
+    "Gnayeru"
+ end
+end
+end
+
+
