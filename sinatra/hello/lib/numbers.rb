99bb22d4bdb8d650564be07bb02638c37151855e
railsfactory-sujitha <railsfactory-sujitha@users.noreply.github.com>
2014-08-01 10:12:19 +0530

    Sinatra Program for Calendar

--- sinatra/hello/lib/numbers.rb
+++ sinatra/hello/lib/numbers.rb
@@ -1,73 +1,20 @@
 class Numbers
 
-  def self.convert(number)
-   num = number.to_i
- if num == 1
-   s = 'first'
-  elsif num == 2
-    s ='second'
-  elsif num == 3
-     s ='third'
- elsif num == 4
-    s = 'fourth'
- elsif num == 5
-    s = 'fifth'
-  elsif num == 6
-    s = 'sixth'
- elsif num == 7
-   s = 'seventh'
-  elsif num ==8
-   s = 'eighth'
-  elsif num == 9
-   s = 'nineth'
-  elsif num == 10
-   s = 'tenth'
-elsif num == 11
-   s = 'eleventh'
-  elsif num == 12
-    s ='twelveth'
-  elsif num == 13
-     s ='thirteenth'
- elsif num == 14
-    s = 'fourteenth'
- elsif num == 15
-    s = 'fifteenth'
-  elsif num == 16
-    s = 'sixteenth'
- elsif num == 17
-   s = 'seventeenth'
-  elsif num == 18
-   s = 'eighteenth'
-  elsif num == 19
-   s = 'nineteenth'
-  elsif num == 20
-   s = 'twentyth'
-elsif num == 21
-   s = 'twentyfirst'
-  elsif num == 22
-    s ='twentysecond'
-  elsif num == 23
-     s ='twentythird'
- elsif num == 24
-    s = 'twentyfourth'
- elsif num == 25
-    s = 'twentyfifth'
-  elsif num == 26
-    s = 'twentysixth'
- elsif num == 27
-   s = 'twentyseventh'
-  elsif num == 28
-   s = 'twentyeigth'
-  elsif num == 29
-   s = 'twentynineth'
-  elsif num == 30
-   s = 'thirtyth'
- elsif num == 31
-  s = 'thirtyfirst'
-else num > 31
+  def self.convert(n)
+
+if (1..31) === n
+case n
+when 1,21,31
+s = "#{n}"+'st'
+when 2,22
+s = "#{n}"+'nd'
+when 3,23
+s = "#{n}"+'rd'
+else
+s = "#{n}"+'th'
+end
+else n > 31
   s= ''
-   
-  end
-  
-  end
+end
+end
 end 
