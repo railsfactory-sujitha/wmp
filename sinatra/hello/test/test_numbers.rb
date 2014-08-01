b02f01c034b9fdb1531e27c57a40b5f726d9cd93
railsfactory-sujitha <railsfactory-sujitha@users.noreply.github.com>
2014-08-01 10:11:37 +0530

    Sinatra Program for Calendar

--- sinatra/hello/test/test_numbers.rb
+++ sinatra/hello/test/test_numbers.rb
@@ -7,10 +7,14 @@ class TestNumbers < Test::Unit::TestCase
 
 
   def test_num
-    assert_equal 'first',Numbers.convert(1)
-    assert_equal 'second',Numbers.convert(2)
-    assert_equal 'sixth',Numbers.convert(6)
-    assert_equal 'thirtyfirst',Numbers.convert(31)
+    assert_equal '1st',Numbers.convert(1)
+    assert_equal '2nd',Numbers.convert(2)
+    assert_equal '6th',Numbers.convert(6)
+    assert_equal '16th',Numbers.convert(16)
+     assert_equal '18th',Numbers.convert(18)
+    assert_equal '22nd',Numbers.convert(22)
+    assert_equal '26th',Numbers.convert(26)
+    assert_equal '31st',Numbers.convert(31)
      assert_equal '',Numbers.convert(40)
   end
 
